4565f9e19658425d951280385385b42e4e679ab8
khdegraffe <khdegraffe@gmail.com>
2025-05-17 20:51:49 -0400

    add html && js files

---
+++ conditionalPractice.js
@@ -0,0 +1,16 @@
+let testGrade = Math.floor(Math.random() * 100);
+console.log(testGrade);
+let studentName = "Poppy";
+if (testGrade <= 64) {
+  console.log(`${studentName} received a F.`);
+} else if (testGrade <= 70) {
+  console.log(`${studentName} recieved a D.`);
+} else if (testGrade <= 79) {
+  console.log(`${studentName} recieved a C.`);
+} else if (testGrade <= 89) {
+  console.log(`${studentName} recieved a B.`);
+} else {
+  console.log(`${studentName} received an A!!`);
+}
+
+//MILITARY EXERCISE
