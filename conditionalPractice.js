135aed979a73973d98e29d19c22a1079ce03c9d3
khdegraffe <khdegraffe@gmail.com>
2025-05-23 20:36:35 -0400

    add fucntion challenges

--- conditionalPractice.js
+++ conditionalPractice.js
@@ -196,3 +196,65 @@ console.log(resultArray);
 
 let resultString = resultArray.join("").toUpperCase();
 console.log(resultString);
+
+//FUNCTION CHALLENGE 1: passwordValidator
+
+function isValidPassword(password, username) {
+  if (password.length < 8) {
+    return false;
+  } else if (password.indexOf(username) !== -1) {
+    return false;
+  } else if (password.indexOf(" ") !== -1) {
+    return false;
+  } else {
+    return true;
+  }
+}
+//FUNCTION CHALLENGE 2: Average
+
+function avg(arr) {
+  let total = 0;
+  for (let num of arr) {
+    total += num;
+  }
+  return total / arr.length;
+}
+
+//FUNCTION CHALLENGE 3: Pangram
+
+function isPangram(sentence) {
+  let lowerCased = sentence.toLowerCase();
+  for (let char of "abcdefghijklmnopqrstuvwxyz") {
+    if (lowerCased.indexOf(char) === -1) {
+      return false;
+    }
+  }
+  return true;
+}
+
+//FUNCTION CHALLENGE 4: Playing Cards
+
+function pick(arr) {
+  const idx = Math.floor(Math.random() * arr.length);
+  return arr[idx];
+}
+
+function getCard() {
+  const values = [
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K",
+    "A",
+  ];
+  const suits = ["clubs", "spades", "hearts", "diamonds"];
+  return { value: pick(values), suit: pick(suits) };
+}
